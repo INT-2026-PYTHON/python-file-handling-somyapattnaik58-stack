ee6eaa395b4416ef64d5ec67dee288ad198c4d1f
somyapattnaik58-stack <somyapattnaik58@gmail.com>
2026-06-11 18:27:35 +0530

    Update words_with_all_vowels.py

--- file_reading_practice/words_with_all_vowels.py
+++ file_reading_practice/words_with_all_vowels.py
@@ -40,3 +40,15 @@ Explanation:
 =================================================
 
 """
+vowels = {'a', 'e', 'i', 'o', 'u'}
+count = 0
+
+with open("sowpods.txt", "r") as file:
+    for word in file:
+        word = word.strip().lower()
+
+        if vowels.issubset(set(word)):
+            print(word)
+            count += 1
+
+print("Total words with all vowels:", count)
